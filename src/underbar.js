cc17388ca2149ffcc279881d735d62dcda92c5d4
Hongxu Liu <djshu.us@gmail.com>
2013-09-13 18:08:18 -0700

    Correctly wrote _.first function

--- src/underbar.js
+++ src/underbar.js
@@ -16,6 +16,9 @@ var _ = { };
   // Return an array of the first n elements of an array. If n is undefined,
   // return just the first element.
   _.first = function(array, n) {
+	  if(n > 0) return array.slice(0, n);
+	  else if(n === 0) return [];
+	  else return array.shift();
   };
 
   // Like first, but for the last elements. If n is undefined, return just the
